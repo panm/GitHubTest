83d643b1957abf132c3b296a4ebdf4a5331f2b9e
panm <panm@outlook.com>
2014-08-01 16:23:49 -0700

    Initial Commit

---
+++ src/my/coding/Algo.java
@@ -0,0 +1,15 @@
+package my.coding;
+
+public class Algo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.print("hello there");
+	}
+
+}
+
+
---
+++ src/my/coding/Trees.java
@@ -0,0 +1,13 @@
+package my.coding;
+
+public class Trees {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
